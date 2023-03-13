d60c4c5d4d2704fff773959f94facb53d923a0cc
developiaa <developiaa@gmail.com>
2023-03-13 23:51:29 +0900

    feat: 오브젝트: chapter5 티켓 판매

---
+++ book/object/chapter5/_01_ticket/Invitation.java
@@ -0,0 +1,7 @@
+package book.object.chapter5._01_ticket;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+}
---
+++ book/object/chapter5/_01_ticket/Ticket.java
@@ -0,0 +1,13 @@
+package book.object.chapter5._01_ticket;
+
+public class Ticket {
+    private Long fee;
+
+    public Ticket(Long fee) {
+        this.fee = fee;
+    }
+
+    public Long getFee() {
+        return fee;
+    }
+}
